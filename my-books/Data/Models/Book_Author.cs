33e503ed45d66a051956662d07ae032c0fc7db07
Ismail Abdul-Hadi <ismail@omnisoft.us>
2021-03-03 21:10:50 -0800

    This is my 2nd commit

---
+++ my-books/Data/Models/Book_Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Models
+{
+    public class Book_Author
+    {
+        public int Id { get; set; }
+
+        //navigation properties for the book
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        //navigation properties for Author
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+    }
+}
